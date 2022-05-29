3bd9e677fe8498078d3ede2cb52e692e39990979
Gustavo <gordono@unsa.edu.pe>
2022-05-28 22:26:55 -0500

    Request and Response

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+def post_list(request):
+    return HttpResponse("Hello World from Django!")
