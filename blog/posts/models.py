28df6552218f1bbc782285404d6abf6421062c23
Gustavo <gordono@unsa.edu.pe>
2022-05-29 21:46:37 -0500

    Models and ORM

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=127, verbose_name="Title")
+    description = models.CharField(max_length=255)
+    img = models.CharField(max_length=511)
+    content = models.TextField()
+    created_date = models.DateField(auto_now_add=True)
