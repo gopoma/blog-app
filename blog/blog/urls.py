03643397ea6f6b5db28c63bf81bc4d83c74b1cb8
Gustavo <gordono@unsa.edu.pe>
2022-05-30 22:36:22 -0500

    Templates, Consultas y Bootstrap

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import posts
+
+from posts.views import home, posts, post
 
 urlpatterns = [
+    path('', home), # / Home
     path('admin/', admin.site.urls),
-    path('posts/', posts)
+    path('posts/', posts),
+    path('posts/<int:id>', post)
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -3,8 +3,20 @@ from django.http import HttpResponse
 from .models import Post
 
 # Create your views here.
+def home(request):
+    return render(request, "home.html")
+
 def posts(request):
     posts = Post.objects.all() # SELECT * FROM posts
     
-    print(posts)
-    return HttpResponse("Posts")
+    # print(posts)
+    # return HttpResponse("Posts")
+
+    return render(request, "posts.html", context={
+        "posts": posts
+    })
+
+def post(request, id):
+    post = Post.objects.get(id=id)
+
+    return render(request, "post.html", {"post": post})
